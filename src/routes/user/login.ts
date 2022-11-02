2eac557bbd8430111693593edcfefa497f07ac47
Jose Casas <20496401+PnsDev@users.noreply.github.com>
2022-11-01 19:29:55 -0500

    Removed login comment

--- src/routes/user/login.ts
+++ src/routes/user/login.ts
@@ -3,23 +3,8 @@ import User from "../../types/user";
 import response from "../../utils/response";
 
 export default async function(req: Request, res: Response) : Promise<any> {
-    
-    // get username and pass from the request body
-    // find the user in the database based on the password and username
-    // if the user is found then generate a token and send it back to the user
-    /**
-     * {
-     *    successful: true,
-     *    token: "token"
-     * }
-     */
-
-    // Save the author
-
     if (req.body.username === undefined || req.body.password === undefined)
         return res.status(400).send(response(false, 'Invalid username or password'));
-
-
 
     let user = await User.fromUserAndPass(req.body.username, req.body.password);
 
