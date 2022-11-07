aabcaac7415ab9d97dc0e79851aeffdf25d7bb94
Jose Casas <20496401+PnsDev@users.noreply.github.com>
2022-11-07 14:39:26 -0600

    Upstreamed authentication by user login

--- src/utils/authChecker.ts
+++ src/utils/authChecker.ts
@@ -1,3 +1,11 @@
-export default (token: String) : Boolean => {
-    return true;
+import users from "../mongoDB/schemas/users";
+
+export default async (token: string | undefined) : Promise<Boolean> => {
+    if (token === undefined) return false;
+    return new Promise((resolve) => {
+        users.findOne({token: token}, (err: any, user: any) => {
+            if (err || user === null) return resolve(false);
+            return resolve(user.admin === true);
+        });
+    });
 };
